93b9426f64976048bc21add66b8fb724d5521e0b
XeonDead <xeondead@gmail.com>
2018-10-24 18:51:12 +0300

    There's no output file? We should create it.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include <sys/stat.h>
 
 using namespace std;
-namespace fs = std::filesystem;
+namespace fs = std::filesystem; //this is c++17 at work, but boost_filesystem is essentially same
 
 int main( int argc , char *argv[ ] ) {
     //initialize empty paths
@@ -24,10 +24,13 @@ int main( int argc , char *argv[ ] ) {
     #endif
     //Nothing to do if we're not given an output file
     if (outputfile=="") {cout << "Only input file specified" << endl; return 1;}
-    //start up with filesystem (c++17)
+    //start up with filesystem (c++17/boost_filesystem)
     fs::path inputpath = fs::u8path(inputfile);
     fs::path outputpath = fs::u8path(outputfile);
+    ofstream file(outputfile); //and no other way
+    fs::resize_file(outputpath, 1024);
     fs::permissions(outputpath, fs::status(inputpath).permissions(), fs::perm_options::replace);
     fs::last_write_time(outputpath,fs::last_write_time(inputpath));
+    //fs::remove(inputpath); will be once we're sure
     return 0;
 }
