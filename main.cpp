a02c55e9068f377646d6ea58f9ec6d374dee312d
XeonDead <xeondead@gmail.com>
2018-10-28 17:49:48 +0300

    Some more changes 1. Fix vscode settings for spacing 2. Update gitignore (some test cases on my side) 3. Add the InvertFile and PreprocessFile functions 4. Add inbuilt command-line documentation 5. Use direct chunk file size instead of general chunk size due to how glueing works

--- main.cpp
+++ main.cpp
@@ -1,38 +1,67 @@
 #include <iostream>
 #include <chrono>
-#include <iomanip>
-#include <fstream>
 #include <filesystem>
 #include <thread>
 #include <mutex>
-#include <cstdlib>
-#include <stdio.h>
+#include <algorithm>
+#include <fstream>
 #include <ios>
-#include <sys/stat.h>
+#include <iterator>
+#include <vector>
 
-#define DEBUG
+//#define DEBUG
 
 using namespace std;
 namespace fs = std::filesystem; //this is c++17 at work, but boost_filesystem is essentially same
 
+string InvertFile(string *File) {
+    ifstream input_file(File->c_str(), ios::binary);
+    ofstream output_file("tmpReversedFile", ios::binary);
+    istreambuf_iterator<char> input_begin(input_file);
+    istreambuf_iterator<char> input_end;
+    ostreambuf_iterator<char> output_begin(output_file);
+    vector<char> input_data(input_begin, input_end);
+
+    reverse_copy(input_data.begin(), input_data.end(), output_begin);
+
+    return "tmpReversedFile";
+}
+
+string PreprocessFile(string *OrigFile, int Operation){
+    string tmpCompressedFile; string tmpEncryptedFile;
+    switch (Operation) {
+        //compress+encrypt stub
+        case(3):tmpCompressedFile=PreprocessFile(OrigFile,1);
+                tmpEncryptedFile=PreprocessFile(&tmpCompressedFile,2);
+                return tmpEncryptedFile;
+        //compress stub
+        case(1):tmpCompressedFile=*OrigFile; return tmpCompressedFile;
+        //encrypt stub
+        case(2):tmpEncryptedFile=*OrigFile; return tmpEncryptedFile;
+        //invert
+        case(4):return InvertFile(OrigFile);
+        default: return *OrigFile;
+    }
+}
+
 string IntToString(int Int){
     //this function is written to purely translate int to string without weak conversions (C-like itoa)
-    std::string ResString;
-    std::stringstream temp_str;
+    string ResString;
+    stringstream temp_str;
     temp_str<<Int;
     ResString = temp_str.str();
     return ResString;
 }
 
-void MakeChunk(int* ChunkNum, int* ChunkSize, string* inputfile, string* outputfile) {
-    //this function grabs a numbered chunk from inputfile and creates it at the destination
+void MakeChunk(int* ChunkNum, int* ChunkSize, string* InputFile, string* OutputFile) {
+    //this function grabs a numbered chunk from InputFile and creates it at the destination
     #ifdef DEBUG
     cout << "MakeChunk " << *ChunkNum << endl;
     #endif
     char *buffer = new char[*ChunkSize];
     ofstream ChunkFile;
-    ifstream infile(inputfile->c_str());
-    string Chunk=*outputfile;
+    ifstream infile(InputFile->c_str());
+    string Chunk=*OutputFile;
     Chunk.append(".");
     Chunk.append(IntToString(*ChunkNum));
     ChunkFile.open(Chunk.c_str(),ios::out | ios::trunc | ios::binary);
@@ -48,22 +77,22 @@ void MakeChunk(int* ChunkNum, int* ChunkSize, string* inputfile, string* outputf
     delete[] buffer;
 }
 
-void GlueChunks (string* ChunkName, string* OutputFile, int* ChunkSize, int* thrCount) {
+void GlueChunks (string* OutputFile, int* ThreadCount) {
     //this function glues the resulting chunks to one output file
     string fileName;
-    ofstream outputfile;
-    outputfile.open(*OutputFile, ios::out | ios::binary);
+    ofstream OutFile;
+    OutFile.open(*OutputFile, ios::out | ios::binary);
 
-    if (outputfile.is_open()) {
+    if (OutFile.is_open()) {
         #ifdef DEBUG
         cout << *OutputFile << " opened to write" << endl;
         #endif
 
-        for (int i=0;i<=*thrCount;i++)
+        for (int i=0;i<=*ThreadCount;i++)
         {
             // Build the filename
             fileName.clear();
-            fileName.append(*ChunkName);
+            fileName.append(*OutputFile);
             fileName.append(".");
             fileName.append(IntToString(i));
 
@@ -75,17 +104,18 @@ void GlueChunks (string* ChunkName, string* OutputFile, int* ChunkSize, int* thr
             #endif
 
             // If chunk opened successfully, read it and write it to the output file.
-            if (fileInput.is_open()) {
-                //we can work around without chunk size (by checking file size) but it's easier to give it to the function instead
-                char *inputBuffer = new char[*ChunkSize];
-                fileInput.read(inputBuffer,*ChunkSize);
-                outputfile.write(inputBuffer,*ChunkSize);
+            if (fileInput.is_open() && fs::file_size(fs::u8path(fileName))!=0) {
+                int ChunkSize = fs::file_size(fs::u8path(fileName));
+                char *inputBuffer = new char[ChunkSize];
+                // After a little brainstorm, using actual chunk size is a better idea
+                fileInput.read(inputBuffer,ChunkSize);
+                OutFile.write(inputBuffer,ChunkSize);
                 delete[](inputBuffer);
                 fileInput.close();
             }
         }
         // Close output file.
-        outputfile.close();
+        OutFile.close();
         #ifdef DEBUG
         cout << "File assembly complete!" << endl;
         #endif
@@ -94,71 +124,77 @@ void GlueChunks (string* ChunkName, string* OutputFile, int* ChunkSize, int* thr
 }
 
 int main( int argc , char *argv[ ] ) {
-    //initialize empty paths
-    std::string inputfile = "", outputfile = "";
-    //mutex for thread-locking
-    std::mutex mtx;
-    //initial thread count and chunk size
-    int thrCount=5;int ChunkSize=0;
-    //populate them from command-line
+        //initialize empty paths
+    string InputFile = "", AttribFile = "", OutputFile = "";
+        //mutex for thread-locking
+    mutex mtx;
+        //initial thread count and chunk size
+    int ThreadCount=5;int ChunkSize=0;int OperationMode=0;
+        //populate them from command-line
+    if (argc<2){
+        cout << "Usage: filemover $InputFile $OutputFile $ThreadCount $OperationMode" << endl;
+        cout << "OperationMode: 1=compress; 2=encrypt; 3=compress+encrypt; 4=reverse" << endl;
+        return 0;
+    }
     if (argc > 2)
     {
-        inputfile = argv[1];
-        outputfile = argv[2];
+        InputFile = argv[1];
+        OutputFile = argv[2];
     }
-    //Nothing to do if we're not given an output file
-    if (outputfile=="") {cout << "Only input file specified" << endl; return 1;}
-    if (argc > 3) {thrCount=atoi(argv[3]);};
-    //TBA: fourth argument for archival, encryption, reversal
-    std::vector<std::thread> threadsPool(thrCount);
-    fs::path inputpath = fs::u8path(inputfile);
-    fs::path outputpath = fs::u8path(outputfile);
+        //Nothing to do if we're not given an output file
+    if (OutputFile=="") {cout << "Only input file specified" << endl; return 1;}
+    if (argc > 3) {ThreadCount=atoi(argv[3]);};
+    if (argc > 4) {OperationMode=atoi(argv[4]);};
     #ifdef DEBUG
-    cout << inputfile << endl;
-    cout << outputfile << endl;
+    cout << "InputFile: " << InputFile << endl;
+    cout << "OutputFile: " <<  OutputFile << endl;
     #endif
-    //this is needed since we can accidentally end up with 0 data in chunks
-    if (thrCount>(fs::file_size(inputpath)/2)) {cout << "Chunks too small to contain any data" << endl;; return 1;}; 
-    if ((fs::file_size(inputpath)%thrCount)==0) 
-    {
-        ChunkSize = (fs::file_size(inputpath)/thrCount); 
-    } 
-    else 
-    {
+        //If OperationMode is not 0, we need to take original file's attributes instead of the new InputFile
+    AttribFile=InputFile;
+    InputFile=PreprocessFile(&InputFile,OperationMode);
+    vector<thread> threadsPool(ThreadCount);
+    fs::path InputPath = fs::u8path(AttribFile);
+    fs::path OutputPath = fs::u8path(OutputFile);
+        //this is needed since we can accidentally end up with 0 data in chunks
+    if (ThreadCount>(fs::file_size(InputPath)/2)) {cout << "Chunks too small to contain any data" << endl;; return 1;}; 
+    if ((fs::file_size(InputPath)%ThreadCount)==0) 
+    {ChunkSize = (fs::file_size(InputPath)/ThreadCount); } 
+    else {ChunkSize = (fs::file_size(InputPath)/ThreadCount)+1; }
         // If file size does not evenly divide in our chunk/thread count, we add one more byte to the chunk size 
-        // this is taken from consideration where file size divided and multiplied by the thread count always bigger than the file
-        // so if we just extend the chunk a little it would not hurt
-        ChunkSize = (fs::file_size(inputpath)/thrCount)+1; 
-    }
-    //the section above might produce a bogus and empty chunk, but that's the price I'm willing to pay to be sure all the data was taken with
+        // this is taken from consideration where if file size has a modulus after division we can be certain that if chunk size will be bigger it will cover
+        // the entire file so if we just extend the chunk a little it will contain the file entirely
     #ifdef DEBUG
     cout << "Size " << ChunkSize << endl;
     #endif
-    for (int i=0;i<=thrCount;i++) {
+    for (int i=0;i<=ThreadCount;i++) {
         //critical section - mutex to avoid race condition and multiple creation of the same chunks
         mtx.lock();
         #ifdef DEBUG
         cout << "Thread " << i << endl;
         #endif
-        threadsPool[i] = thread(&MakeChunk,&i,&ChunkSize,&inputfile,&outputfile);
+        threadsPool[i] = thread(&MakeChunk,&i,&ChunkSize,&InputFile,&OutputFile);
         threadsPool[i].join();
         mtx.unlock();
     }
-    //we need the exitfile since we have chunks to merge to it
-    ofstream offile(outputfile);
-    GlueChunks(&outputfile,&outputfile,&ChunkSize,&thrCount);
-    //finish up with file system (c++17/boost_file system)
-    fs::permissions(outputpath, fs::status(inputpath).permissions(), fs::perm_options::replace);
-    fs::last_write_time(outputpath,fs::last_write_time(inputpath));
-    #ifdef DELETE
-    fs::remove(inputpath);
+        //we don't need any tmp files past this point so we can safely remove them
+    fs::remove("tmpCompressedFile");
+    fs::remove("tmpEncryptedFile");
+    fs::remove("tmpReversedFile");
+        //we need the exitfile since we have chunks to merge to it
+    ofstream offile(OutputFile);
+    GlueChunks(&OutputFile,&ThreadCount);
+        //finish up with file system (c++17 filesystem/boost_filesystem)
+    fs::permissions(OutputPath, fs::status(InputPath).permissions(), fs::perm_options::replace);
+    fs::last_write_time(OutputPath,fs::last_write_time(InputPath));
+    #ifndef DEBUG
+    fs::remove(InputPath);
     #endif
-    for (int i=0;i<=thrCount;i++)
+    for (int i=0;i<=ThreadCount;i++)
     {
         //clean up
-        std::string ChunkName;
+        string ChunkName;
         ChunkName.clear();
-        ChunkName.append(outputfile);
+        ChunkName.append(OutputFile);
         ChunkName.append(".");
         ChunkName.append(IntToString(i));
         #ifdef DEBUG
